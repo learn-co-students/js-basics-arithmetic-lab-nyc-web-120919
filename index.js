561bc94922c1b70434e8c37f0831ab4063ec9825
Ben Kagan <kagan.benjamin@gmail.com>
2020-01-27 15:33:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here
+let var1 = 2;
+let var2 = 31; 
+
+let multiply = var1 * var2; 
+
+let random = Math.floor(Math.random() * 10) + 1; 
+
+let mod = 24 % 5; 
+
+const max = Math.max(1, 2, 3, 4, 20);
